543d4b2958c1939c9acf0a621701c94c11f62fd0
yuriolive <yuriso1994@gmail.com>
2015-10-07 01:58:21 -0300

    v2 of face population
    v2 of face population

--- face.php
+++ face.php
@@ -1,83 +1,6 @@
 <?php
     $logFile = fopen("tbface.log", "w");
 
-    function insercao($place, $my_connect, $context) {
-        if(isset($place["city"]) && $place["city"] != NULL) {
-            $json = file_get_contents(
-                "https://graph.facebook.com/search?q=". urlencode($place["name"] . " " . $place["city"]) ."&type=page&access_token=1629898650614051|mrLeYR0bO0ym2eIRnzLKp0NZrxU",
-                false,
-                $context
-            );
-        } else {
-            $json = file_get_contents(
-                "https://graph.facebook.com/search?q=". urlencode($place["name"] . " Sao Paulo") ."&type=page&access_token=1629898650614051|mrLeYR0bO0ym2eIRnzLKp0NZrxU",
-                false,
-                $context
-            );
-        }
-
-        $fb_places = json_decode($json, true);
-
-
-        if(isset($fb_places["error"])) {
-            fwrite($logFile, $place["place_id"] . "\n");
-            die();
-        }
-        
-        sleep(1);
-
-        $encontrou = 0;
-        foreach ($fb_places["data"] as $fb_place) {
-            $json_place = file_get_contents(
-                "https://graph.facebook.com/" . $fb_place["id"] . "?access_token=1629898650614051|mrLeYR0bO0ym2eIRnzLKp0NZrxU&fields=location,about,category,name,likes,checkins",
-                false,
-                $context
-            );
-
-            if(isset($json_place["error"])) {
-                fwrite($logFile, $place["place_id"] . "\n");
-                die();
-            }
-
-            $candidate_place = json_decode($json_place, true);
-            
-            if(number_trim($place["lat"] * 1000) == number_trim($candidate_place["location"]["latitude"] * 1000) && number_trim($place["lng"] * 1000) == number_trim($candidate_place["location"]["longitude"] * 1000)) {
-                if(isset($place["city"]) && $place["city"] != NULL) {
-                    mysql_query(
-                        "UPDATE tbgoogle SET
-                        facebook_ID='" . $candidate_place['id'] . "', 
-                        likes='" . $candidate_place['likes'] . "', 
-                        checkins='" . $candidate_place['checkins'] . "'
-                        WHERE place_id='" . $place["place_id"] . "'
-                        ;"
-                    ); 
-                } else {
-                    mysql_query(
-                        "UPDATE tbgoogle SET
-                        city='" . $candidate_place['location']['city'] . "', 
-                        facebook_ID='" . $candidate_place['id'] . "', 
-                        likes='" . $candidate_place['likes'] . "', 
-                        checkins='" . $candidate_place['checkins'] . "'
-                        WHERE place_id='" . $place["place_id"] . "'
-                        ;"
-                    );
-                }
-                $encontrou = 1;
-                break;
-            }
-            sleep(1);
-        }
-        return $encontrou;
-    }
-
-    function number_trim($x) {
-        if ($x < 0)
-            return ceil($x);
-        else
-            return floor($x);
-    }
-
-
 	$my_connect = mysql_connect("localhost","root","");
 	if (!$my_connect) {	die('Error connecting to the database: ' . mysql_error()); }
     mysql_select_db("black_onion", $my_connect);
@@ -88,35 +11,91 @@
 	mb_internal_encoding("UTF-8");
 	$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
     
-    $query_db = "SELECT place_id, name, city, lat, lng FROM tbgoogle ORDER BY rating";
+    $query_db = "SELECT place_id, name, city, lat, lng FROM tbgoogle WHERE facebook_ID IS NULL OR facebook_ID='0' ORDER BY rating";
     $places_res = mysql_query($query_db, $my_connect);
 
     if($places_res === FALSE) { die(mysql_error()); }
 
     while($place = mysql_fetch_array($places_res)) {
+        firstSearchType:
+        $json = file_get_contents(
+                "https://graph.facebook.com/search?center=" . $place['lat'] . "," . $place['lng'] . 
+                "&distance=100&fields=id,name,category,category_list,about,bio,description,general_info,location,talking_about_count,were_here_count,likes,phone,cover,website&type=place&access_token=1629898650614051|mrLeYR0bO0ym2eIRnzLKp0NZrxU",
+                false,
+                $context
+            );  
         
-        if(insercao($place, $my_connect, $context) == 0) {
-            $str_vector = explode(" ",  $place['name']);
-            $str_quant = count($str_vector);
+        $possible_places = json_decode($json, true);
+        
+        if(isset($possible_places['error'])) {
+            sleep(1);
+            goto firstSearchType;
+        }
             
-            foreach ($str_vector as $newName) {
-                $place['name'] = $newName;
-                if(insercao($place, $my_connect, $context) == 1) {
-                    goto saiu;
-                }
+        $count = count($possible_places['data']);
+        if($count <= 0) {
+            secondSearchType:
+            $json = file_get_contents(
+                "https://graph.facebook.com/search?center=" . $place['lat'] . "," . $place['lng'] . 
+                "&distance=100&fields=id,name,category,category_list,about,bio,description,general_info,location,talking_about_count,were_here_count,likes,phone,cover,website&q=" .
+                urlencode($place['q']) .
+                "&type=place&access_token=1629898650614051|mrLeYR0bO0ym2eIRnzLKp0NZrxU",
+                false,
+                $context
+            );  
+      
+            $possible_places = json_decode($json, true);
+            
+            if(isset($possible_places['error'])) {
+                sleep(1);
+                goto secondSearchType;
             }
             
-            for($i = 0; $i < $str_quant - 1; $i++) { // Combinacao de $str_quant 2 a 2
-                for($j = $i+1; $j < $str_quant; $j++) {
-                    $place['name'] = $str_vector[$i] . " " . $str_vector[$j];
-                    if(insercao($place, $my_connect, $context) == 1) {
-                        goto saiu;
-                    }
+            $count = count($possible_places['data']);
+            if ($count > 0) {
+                $place_FB = $possible_places['data'][0];
+                similar_text($place['name'], $place_FB['name'], $perc);
+                for($i = 1; $i < $count; $i++) {
+                     similar_text($place['name'], $possible_places['data'][$i]['name'], $auxPerc);
+                     if($auxPerc > $perc) {
+                        $perc = $auxPerc;
+                        $place_FB = $possible_places['data'][$i]['name'];
+                     }
                 }
+                if($perc > 50) {
+                    mysql_query(
+                        "UPDATE tbgoogle SET
+                        facebook_ID='" . $place_FB['id'] . "', 
+                        likes='" . $place_FB['likes'] . "', 
+                        checkins='" . $place_FB['were_here_count'] . "',
+                        name_FB='" . $place_FB['name'] . "'
+                        WHERE place_id='" . $place["place_id"] . "'
+                        ;"
+                    );
+                } 
+            }
+        } else {
+            $place_FB = $possible_places['data'][0];
+            similar_text($place['name'], $place_FB['name'], $perc);
+            for($i = 1; $i < $count; $i++) {
+                 similar_text($place['name'], $possible_places['data'][$i]['name'], $auxPerc);
+                 if($auxPerc > $perc) {
+                    $perc = $auxPerc;
+                    $place_FB = $possible_places['data'][$i]['name'];
+                 }
+            }
+            if($perc > 50) {
+                mysql_query(
+                    "UPDATE tbgoogle SET
+                    facebook_ID='" . $place_FB['id'] . "', 
+                    likes='" . $place_FB['likes'] . "', 
+                    checkins='" . $place_FB['were_here_count'] . "',
+                    name_FB='" . $place_FB['name'] . "'
+                    WHERE place_id='" . $place["place_id"] . "'
+                    ;"
+                );
             } 
         }
-
-        saiu:
     }    
    
     mysql_close($my_connect);
