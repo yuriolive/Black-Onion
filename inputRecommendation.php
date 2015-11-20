4d003ae26dded4bc3091a800275e6c2e88e313c5
Gabriel-Siqueira <gabriel._gabriel@hotmail.com>
2015-11-20 19:10:53 -0200

    input zuado

--- inputRecommendation.php
+++ inputRecommendation.php
@@ -118,31 +118,35 @@ class Recommendation_long_term_by_input extends Recommendation_short_term_by_inp
        $artistas = array();        // artists that the user may like
   
   function __construct($lat, $lng, $input) {
-      parent::__construct($lat, $lng);
+      parent::__construct($lat, $lng);      
       $this->input = $input;
-      $this->find_artist();
+      $this->find_artist($input);
+      var_dump($input);
   }
     
   public function reset_recommendation() {
       parent::reset_recommendation();
-      $this->find_artist();
+      $this->find_artist(NULL);
   }
 
   /* find artists indirectly releted with the user */
-  public function find_artist() {
-      
+    public function find_artist ($input) {
+     var_dump( $input);        
     $query_db = "SELECT id_fb FROM artista WHERE LOWER(nome_artista) LIKE LOWER('%".$this->input."%')" ;
     $artistas_input_res = mysql_query($query_db, $this->my_connect);
     if($artistas_input_res === FALSE) { die(mysql_error()); }
     while($artistas_row = mysql_fetch_array($artistas_input_res)) {
         array_push($this->artistas, $artistas_row['id_fb']);
     }
-
+    
     if(is_array($this->artistas)) { 
         $this->artistas = array_unique($this->artistas);
         $this->artistas = array_filter($this->artistas, 'strlen');
+       
+        
         foreach($this->artistas as $artista) {
             $query_db = "SELECT a2.id_fb FROM artista a1, artista a2, assemelha_artista aa WHERE " .mysql_real_escape_string($artista). " = a1.id_fb AND a1.id_spotify = aa.id_spotify_super and aa.id_spotify_sub = a2.id_spotify";
+            //echo "SELECT a2.id_fb FROM artista a1, artista a2, assemelha_artista aa WHERE " .mysql_real_escape_string($artista). " = a1.id_fb AND a1.id_spotify = aa.id_spotify_super and aa.id_spotify_sub = a2.id_spotify";
             $artistas_res = mysql_query($query_db, $this->my_connect);
             if($artistas_res === FALSE) { die(mysql_error()); }
             while($artistas_row = mysql_fetch_array($artistas_res)) {
@@ -184,11 +188,11 @@ class Recommendation_long_term_by_input extends Recommendation_short_term_by_inp
     // use long term recommendation
     $recommendation = $this->long_term($num);
     // complemented with short term recommendation
-    if (count($recommendation) < $num) {
+    /*if (count($recommendation) < $num) {
         $time = time() * 3600 * 24 * 30;
         $recommendation2 = $this->short_term($num - count($recommendation), $time, 0.5);
         $recommendation = array_merge($recommendation, $recommendation2);
-    }
+        }*/
     return $recommendation;
   }
 }
